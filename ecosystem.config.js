e339ea99554ee6b8f411a248c856aae30308cb0a
Wandole <58407717+Wandole@users.noreply.github.com>
2022-05-19 16:29:15 +0300

    Deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "bin/www",
+      watch: ".",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "C:/Users/Maxime/.ssh/id_rsa.pub",
+      user: "root",
+      host: "194.31.150.105",
+      ref: "origin/main",
+      repo: "git@github.com:Wandole/APIHoraire.git",
+      path: "/root/horaireAPI",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
